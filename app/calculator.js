fc148c492bd96fe931e0408e6360172c3cbe4e9a
Brian Kinyua <briankinyua0101@gmail.com>
2025-12-22 16:00:23 +0300

    implement basic calculator functionality handling polish notation

---
+++ app/calculator.js
@@ -0,0 +1,33 @@
+exports.calculate = function(expression) {
+
+  if (!expression||expression.trim() === '') return 0;
+
+  const tokens = expression.trim().split(/\s+/);
+  let currIndex = 0;
+
+  processToken = () => {
+    const token = tokens[currIndex++];
+    const num = parseFloat(token);
+
+    if(!isNaN(num)){
+      return num;
+    }
+
+    const operand1 = processToken();
+    const operand2 = processToken();
+
+    switch(token) {
+      case '+': return operand1 + operand2;
+      case '-': return operand1 - operand2;
+      case '*': return operand1 * operand2;
+      case '/': return operand1 / operand2;
+
+      default: return 0;
+
+    }
+
+
+  }
+
+    return processToken();
+}
